d44bf0644de1293106ba2d9f81c452a27cb8bda0
Yarik MBP14 <yarikyarik20017@gmail.com>
2024-11-20 14:33:42 +0200

    релиз

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+
+            // Получаем текущий год
+            const currentYear = new Date().getFullYear();
+            // Вставляем текущий год в элемент с id="year"
+            document.getElementById('year_creation').textContent = currentYear;
